32f569fb1067592d5821baf1b1549beb3240c297
wonoh <cwo1401@gmail.com>
2022-10-12 21:27:52 +0900

    todo complete

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import Auth from "./pages/Auth";
+import Todo from "./pages/Todo";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Auth />} />
+        <Route path="/todo" element={<Todo />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
